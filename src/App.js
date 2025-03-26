59bfaa13615e184719ad5039d2cd7788a9abc164
savan khedekar <savankhedekar.scaler@gmail.com>
2025-03-26 20:13:45 +0530

    Add Google review component and make website reponsive

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import CoursesPage from "./pages/CoursesPage";
@@ -10,25 +10,24 @@ import TermServicePage from "./pages/TermsServicePage";
 import RefundPage from "./pages/RefundPage";
 import AboutPage from "./pages/AboutPage";
 import ResearchGuidancePage from "./pages/ResearchGuidancePage";
-import WhyUs from "./component/WhyUs";
 
 function App() {
   return (
-          <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/courses" element={<CoursesPage />} />
-              <Route path="/events" element={<EventPage />} />
-              <Route path="/gallery" element={<GalleryPage />} />
-              <Route path="/research" element={<ResearchGuidancePage />} />
-              <Route path="/contact" element={<ContactUsPage />} />
-              <Route path="/terms" element={<TermServicePage />} />
-              <Route path="/privacy" element={<TermServicePage />} />
-              <Route path="/refund" element={<RefundPage />} />
-              <Route path="/about" element={<AboutPage />} />
-              {/*<Route path="/services" element={<WhyUs />} />*/}
-              {/*<Route path="*" element={<NotFound />} />*/}
-          </Routes>
-        );
-        }
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/courses" element={<CoursesPage />} />
+      <Route path="/events" element={<EventPage />} />
+      <Route path="/gallery" element={<GalleryPage />} />
+      <Route path="/research" element={<ResearchGuidancePage />} />
+      <Route path="/contact" element={<ContactUsPage />} />
+      <Route path="/terms" element={<TermServicePage />} />
+      <Route path="/privacy" element={<TermServicePage />} />
+      <Route path="/refund" element={<RefundPage />} />
+      <Route path="/about" element={<AboutPage />} />
+      {/*<Route path="/services" element={<WhyUs />} />*/}
+      {/*<Route path="*" element={<NotFound />} />*/}
+    </Routes>
+  );
+}
 
-        export default App;
+export default App;
--- src/component/Carousel.js
+++ src/component/Carousel.js
@@ -1,38 +1,100 @@
+import { useState, useEffect } from "react";
+
 const Carousel = () => {
-    return (
-        <section id="carousel" className="about">
-            <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
-                <ol className="carousel-indicators">
-                    <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                </ol>
-                <div className="carousel-inner">
-                    {[
-                        "assets/img/Sushodh/Slider 1.jpg",
-                        "assets/img/Sushodh/Slider 2.jpg",
-                        "assets/img/Sushodh/Slider 2a.jpeg",
-                        "assets/img/Sushodh/Slider 3.jpg",
-                        "assets/img/Sushodh/Slider 4.jpg",
-                        "assets/img/Sushodh/Slider 5.jpeg"
-                    ].map((src, index) => (
-                        <div className={`carousel-item ${index === 0 ? "active" : ""}`} key={index}>
-                            <img className="d-block w-65" src={src} alt={`Slide ${index + 1}`} width="100%" height="600vh" />
-                        </div>
-                    ))}
-                </div>
-                <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                    <i style={{ fontSize: "60px" }} className="fas fa-arrow-alt-circle-right"></i>
-                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span className="sr-only">Previous</span>
-                </a>
-                <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                    <span className="carousel-control-next-icon" style={{ color: "red" }} aria-hidden="true"></span>
-                    <span className="sr-only">Next</span>
-                </a>
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return (
+    <section
+      id="carousel"
+      className="about"
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        paddingTop: "0px !important",
+      }}
+    >
+      <div
+        id="carouselExampleIndicators"
+        className="carousel slide"
+        data-ride="carousel"
+        style={{
+          padding: isMobile ? "20px" : "50px",
+          width: isMobile ? "100%" : "75%",
+        }}
+      >
+        <ol className="carousel-indicators">
+          {[...Array(6).keys()].map((index) => (
+            <li
+              key={index}
+              data-target="#carouselExampleIndicators"
+              data-slide-to={index}
+              className={index === 0 ? "active" : ""}
+            ></li>
+          ))}
+        </ol>
+        <div className="carousel-inner">
+          {[
+            "assets/img/Sushodh/Slider 1.jpg",
+            "assets/img/Sushodh/Slider 2.jpg",
+            "assets/img/Sushodh/Slider 2a.jpeg",
+            "assets/img/Sushodh/Slider 3.jpg",
+            "assets/img/Sushodh/Slider 4.jpg",
+            "assets/img/Sushodh/Slider 5.jpeg",
+          ].map((src, index) => (
+            <div
+              className={`carousel-item ${index === 0 ? "active" : ""}`}
+              key={index}
+            >
+              <img
+                className="d-block w-100"
+                src={src}
+                alt={`Slide ${index + 1}`}
+                style={{
+                  height: isMobile ? "auto" : "600px",
+                  maxHeight: isMobile ? "300px" : "none",
+                  width: "100%",
+                }}
+              />
             </div>
-        </section>
-    );
+          ))}
+        </div>
+        <a
+          className="carousel-control-prev"
+          href="#carouselExampleIndicators"
+          role="button"
+          data-slide="prev"
+        >
+          <i
+            style={{ fontSize: "60px" }}
+            className="fas fa-arrow-alt-circle-left"
+          ></i>
+          <span className="sr-only">Previous</span>
+        </a>
+        <a
+          className="carousel-control-next"
+          href="#carouselExampleIndicators"
+          role="button"
+          data-slide="next"
+        >
+          <i
+            style={{ fontSize: "60px" }}
+            className="fas fa-arrow-alt-circle-right"
+          ></i>
+          <span className="sr-only">Next</span>
+        </a>
+      </div>
+    </section>
+  );
 };
 
 export default Carousel;
