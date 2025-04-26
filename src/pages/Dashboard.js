88af596ea2b8d2dd88602accc798c779f3b80be2
Adarsh Agrawal <adarshagrawal968@gmail.com>
2025-04-26 23:44:43 +0530

    add daynamic slider Images

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import DashboardHeader from "../component/DashboardHeader";
+import AddSliderImage from '../component/AddSliderImage';
 
 const Dashboard = () => (
     <div>
         <DashboardHeader/>
-
+        <AddSliderImage />
     </div>
 );
 
